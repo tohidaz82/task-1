b8beb4730071bc6ef911e68317ab175136564d23
TOHID AZIZI <161323615+tohidaz82@users.noreply.github.com>
2024-03-05 18:08:15 +0330

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,11 @@
+t=input("choose (+,-,*,/) = ")
+num1=float(input("write number 1 : "))
+num2=float(input("write number 2 : "))
+if t == '+' :
+    print(num1 + num2)
+elif t == '-' :
+    print(num1 - num2)
+elif t == '*' :
+    print(num1 * num2)
+elif t == '/' :
+    print(num1 / num2)
