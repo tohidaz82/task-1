ca72cc3239d7d433bf964c69fcb9d248b465a852
TOHID AZIZI <tohidazizi.82bw@gmail.com>
2024-03-05 17:24:17 +0330

    Update task-1.py

--- task-1.py
+++ task-1.py
@@ -1,9 +1,7 @@
 t=input("please enter something : ")
 if t[0: ] in '-0123456789':
     print("integer")
-elif 1 :
-    for char in t :
-        if char=="," :
-            print("list")
+elif ',' in t :
+    print("list")
 else :
     print("string")
